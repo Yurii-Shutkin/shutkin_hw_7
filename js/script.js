a4b422c057edf8ea86ab7f4eac7d2876529eaee1
Yurii-Shutkin <itorik@ukr.net>
2021-10-08 02:35:36 +0300

    init

---
+++ js/script.js
@@ -0,0 +1,116 @@
+// Task 1
+
+const a = +prompt ('Чему равно "a" ?');
+alert (a === 0 ? 'Верно' : 'Неверно');
+
+//  Task 2 
+
+// const a = +prompt ('Чему равно "a" ?');
+// alert ( a > 0 ? 'Верно' : 'Неверно');
+
+//Task 3
+
+// const a = +prompt ('Чему равно "a" ?');
+// alert ( a < 0 ? 'Верно' : 'Неверно');
+
+//Task 4
+
+// const a = +prompt ('Чему равно "a" ?');
+// alert ( a >= 0 ? 'Верно' : 'Неверно');
+
+//Task 5 
+
+// const a = +prompt ('Чему равно "a" ?');
+// alert ( a <= 0 ? 'Верно' : 'Неверно');
+
+//Task 6
+
+// const a = +prompt ('Чему равно "a" ?');
+// alert ( a !== 0 ? 'Верно' : 'Неверно');
+
+//Task 7 
+
+// const a = prompt ('Чему равно "a" ?');
+// alert ( a === 'test' ? 'Верно' : 'Неверно');
+
+//Task 8 
+
+// const a = prompt ('Чему равно "a" ?');
+// alert ( a === '1' ? 'Верно' : 'Неверно');
+// console.log (a, typeof a);
+
+//Task 9
+
+// const a = prompt ('Чему равно "a" ?');
+// alert ( a > 0 && a < 5 ? 'Верно' : 'Неверно');
+
+//Task 10
+
+// let a = +prompt ('Чему равно "a" ?');
+// alert (a === 0 || a === 2 ? a += 7 : a /= 10);
+
+// let a = +prompt ('Чему равно "a" ?');
+// if (a === 0 || a === 2) {
+//     alert (a += 7);
+// } else {
+//     alert (a /= 10);
+// };
+
+// let a = +prompt ('Чему равно "a" ?');
+// switch (a) {
+//     case 0:
+//     case 2:
+//         alert (a += 7);
+//         break;
+//     default:
+//         alert (a /= 10);
+// };
+
+//Task 11
+
+// let a = +prompt ('Чему равно "a"?');
+// let b = +prompt ('Чему равно "b"?');
+// alert (a <= 1 && b >=3 ? a + b : a - b);
+
+// let a = +prompt ('Чему равно "a"?');
+// let b = +prompt ('Чему равно "b"?');
+// if (a <= 1 && b >= 3) {
+//     alert (a + b);
+// } else {
+//     alert (a - b);
+// };
+
+//Task 12
+
+// const a = +prompt ('Чему равно "a"?');
+// const b = +prompt ('Чему равно "b"?');
+// alert (a > 2 && a < 11 || b >= 6 && b < 14 ? 'Верно' : 'Неверно');
+
+// const a = +prompt ('Чему равно "a"?');
+// const b = +prompt ('Чему равно "b"?');
+// if (a >2 && a < 11 || b >= 6 && b < 14) {
+//     alert ('Верно');
+// } else {
+//     alert ('Неверно');
+// };
+
+//Task 13
+
+// const num = +prompt ('Введи число от 1 до 4');
+// let result;
+// switch (num) {
+//     case 1:
+//         alert (result = 'Зима');
+//         break;
+//     case 2:
+//         alert (result = 'Весна');
+//         break;
+//     case 3:
+//         alert (result = 'Лето');
+//         break;
+//     case 4:
+//         alert (result = 'Осень');
+//         break;
+//     default:
+//         alert ('Ты ввел число вне диапазона или вовсе не число');
+// };
